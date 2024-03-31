be22867c15cb29821c4631a0d94276f11cc1568a
Филиппов Данил <filippok2004180@gmail.com>
2024-03-31 19:32:16 +0500

    Добавил в cli, проверку, что id число #2

--- fips_fetcher/__main__.py
+++ fips_fetcher/__main__.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         exit('Использование: python -m fips_fetcher {id-документа} {db} {rn}')
     id_: str = sys.argv[1]
-    if not id_.isalnum():
+    if not id_.isdigit():
         exit('ID должен быть числом!')
     print(f"Получаем данные по ссылке {get_final_url(id_, FipsFetcher.default_db, FipsFetcher.default_rn)}")
     res = get_doc(int(id_))
