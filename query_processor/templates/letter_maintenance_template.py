ed6fc670ff2596f17a010e5e3f491a41b0925d5f
Филиппов Данил <filippok2004180@gmail.com>
2024-04-16 21:25:25 +0500

    Заполнение полей в шаблоне с сохранением стилей

--- query_processor/templates/letter_maintenance_template.py
+++ query_processor/templates/letter_maintenance_template.py
@@ -21,16 +21,18 @@ class LetterMaintenanceDocxTemplate(Template):
         output_file = f"../out/Letter_maintenance_{uuid.uuid4()}.docx"
         for paragraph in doc.paragraphs:
             for key, value in fields.items():
-                if f'{{{{ {key} }}}}' in paragraph.text:
-                    paragraph.text = paragraph.text.replace(f'{{{{ {key} }}}}', str(value))
+                for run in paragraph.runs:
+                    if f'{{{{ {key} }}}}' in run.text:
+                        run.text = run.text.replace(f'{{{{ {key} }}}}', str(value))
 
         for table in doc.tables:
             for row in table.rows:
                 for cell in row.cells:
                     for paragraph in cell.paragraphs:
                         for key, value in fields.items():
-                            if f'{{{{ {key} }}}}' in paragraph.text:
-                                paragraph.text = paragraph.text.replace(f'{{{{ {key} }}}}', str(value))
+                            for run in paragraph.runs:
+                                if f'{{{{ {key} }}}}' in run.text:
+                                    run.text = run.text.replace(f'{{{{ {key} }}}}', str(value))
 
         doc.save(output_file)
         return output_file
