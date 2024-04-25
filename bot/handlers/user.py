ad9481ea888db71da843716bbd4f0e91c2d85c9c
Aziz <enietogou@gmail.com>
2024-04-25 21:32:37 +0500

    Добавлены хендлеры для работы с пользователем

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -4,13 +4,44 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.markdown import hbold, hcode, hlink
 from bot.keyboards import inline, reply
+import bot.utils.db_commands as db
+from bot.misc.states import CreateDocument
 
-async def echo_handler(messasge: Message) -> None:
-    try:
-        await messasge.answer(messasge.text, reply_markup=await reply.start_menu())
-    except TypeError:
-        await messasge.answer("!")
+async def user_start(messasge: Message, state: FSMContext) -> None:
+    await state.reset_state()
+    user = await db.get_user()
+    if not user:
+        await db.add_new_user()
+    await messasge.answer("Привет!", reply_markup=await reply.start_menu())
+
+async def create_document(messasge: Message, state: FSMContext) -> None:
+    await state.reset_state()
+    await messasge.answer("Выберите процессор", reply_markup=await inline.select_processor())
+    await CreateDocument.Processor.set()
+    print("Current state: ", await state.get_state())
+    print("ok")
+
+
+async def select_use_yandex_gpt(call: CallbackQuery, callback_data: dict, state: FSMContext) -> None:
+    await state.update_data(processor_number=callback_data.get('processor_number'))
+    await call.message.edit_text("Использовать Yandex GPT?", reply_markup=await inline.select_use_yandex_gpt())
+    await CreateDocument.UseYandexGPT.set()
+
+
+async def push_data(call: CallbackQuery, callback_data: dict, state: FSMContext) -> None:
+    await state.update_data(use_yandex_gpt=callback_data.get('use_yandex_gpt'))
+    await call.message.delete()
+    data = await state.get_data()
+    processor_number = data.get('processor_number')
+    use_yandex_gpt = data.get('use_yandex_gpt')
+    await call.message.answer(f"{hcode(processor_number)}, ЯГПТ: {'Да' if use_yandex_gpt else 'Нет'}"
+                              f"\nНачинаю создание документа", reply_markup=await reply.cancel_menu())
 
 
 def register_user(dp: Dispatcher):
-    dp.register_message_handler(echo_handler)
+    dp.register_message_handler(user_start, commands=["start"], state="*")
+    dp.register_message_handler(user_start, text="Отмена", state="*")
+    dp.register_message_handler(create_document, text="Создать документ", state="*")
+    dp.register_callback_query_handler(select_use_yandex_gpt, inline.processor.filter(action="get_processor"), state=CreateDocument.Processor)
+    dp.register_callback_query_handler(push_data, inline.yandex_gpt.filter(action="get_yagpt"), state=CreateDocument.UseYandexGPT)
+
--- bot/keyboards/inline.py
+++ bot/keyboards/inline.py
@@ -1,2 +1,17 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.callback_data import CallbackData
+
+processor = CallbackData('select_processor', 'action', 'processor_number')
+yandex_gpt = CallbackData('select_yandex_gpt', 'action', 'use_yandex_gpt')
+
+async def select_processor():
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton("Соглашение об обработке персональных данных", callback_data=processor.new("get_processor", "1")))
+    markup.add(InlineKeyboardButton("Обработка писем", callback_data=processor.new("get_processor", "2")))
+    return markup
+
+async def select_use_yandex_gpt():
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton("Да", callback_data=yandex_gpt.new("get_yagpt", True)))
+    markup.add(InlineKeyboardButton("Нет", callback_data=yandex_gpt.new("get_yagpt", False)))
+    return markup
