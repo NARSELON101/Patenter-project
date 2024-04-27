7385b4844aec48304ff74c60d262a94288f85ba8
Филиппов Данил <filippok2004180@gmail.com>
2024-04-27 17:10:08 +0500

    Вынес обработку запросов процессора к пользователю в базовый класс

--- query_processor/processors/processor.py
+++ query_processor/processors/processor.py
@@ -14,5 +14,18 @@ class QueryProcessor:
     def communicate_with_user(self, prompt: str) -> str:
         pass
 
-    async def async_communicate_with_user(self, prompt: str, *args, **kwargs) -> str:
-        return self.communicate_with_user(prompt)
+    async def async_communicate_with_user(self, prompt: str, *args, **kwargs):
+        if hasattr(self, 'tg_dispatcher'):
+            bot = self.tg_dispatcher.bot
+            dispatcher = self.tg_dispatcher
+            state = self.tg_dispatcher.current_state()
+        else:
+            raise AttributeError("tg_dispatcher не найден")
+        if args is not None:
+            sep = ' '
+            if kwargs is not None:
+                sep = kwargs.get('sep', ' ')
+            for arg in args:
+                prompt += sep
+                prompt += str(arg)
+        return await dispatcher.bot.send_message(state.chat, prompt)
