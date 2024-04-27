e086149da2c28aa7e57c62013e3b325049bedd27
Филиппов Данил <filippok2004180@gmail.com>
2024-04-27 11:59:08 +0500

    Добавил в базовый класс QueryProcessor поддержку асинхронного ввода/вывода

--- query_processor/processors/processor.py
+++ query_processor/processors/processor.py
@@ -7,3 +7,12 @@ class QueryProcessor:
 
     def process_query(self, query: str):
         pass
+
+    async def async_process_query(self, query: str):
+        return self.process_query(query)
+
+    def communicate_with_user(self, prompt: str) -> str:
+        pass
+
+    async def async_communicate_with_user(self, prompt: str, *args, **kwargs) -> str:
+        return self.communicate_with_user(prompt)
