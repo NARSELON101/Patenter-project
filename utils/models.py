f620dc3069507e2f1fa7975c5588de31af6e0a1f
AnyaBarmina <55316602+AnyaBarmina@users.noreply.github.com>
2024-04-27 20:59:57 +0500

    Добавлена таблица для файлов

--- utils/models.py
+++ utils/models.py
@@ -1,5 +1,5 @@
 import datetime
-from sqlalchemy import (Column, Integer, DateTime)
+from sqlalchemy import (Column, Integer, DateTime, JSON, String)
 from utils.database import db
 
 
@@ -8,3 +8,9 @@ class User(db.Model):
     id = Column(Integer, autoincrement=True, primary_key=True, unique=True)
     telegram_id = Column(Integer, default=0)
     registation_date = Column(DateTime, default=datetime.datetime.now())
+
+class Files(db.Model):
+    __tablename__ = "available_files"
+    id = Column(Integer, primary_key=True)
+    file_path = Column(String, default="")
+    file = Column(JSON) #default
