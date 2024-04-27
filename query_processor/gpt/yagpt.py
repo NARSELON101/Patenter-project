a71cd468cdec2f854adb004a8a8ae7a6447cf6d5
Филиппов Данил <filippok2004180@gmail.com>
2024-04-27 16:55:38 +0500

    Асинхронная работа yagpt

--- query_processor/gpt/yagpt.py
+++ query_processor/gpt/yagpt.py
@@ -1,13 +1,15 @@
+import logging
+
+import aiohttp
 import requests
 
+from loader import config
 
-class YaGptConfig:
-    catalog_id = 'b1g659p7ld8o2k31g37c'
-    api_key = 'AQVN0ZRFbyqk97EWUy0KAG9jzqs6oUdcXrNYjYJ-'
+logger = logging.getLogger(__name__)
 
 
-def yagpt(query):
-    prompt = {"modelUri": f"gpt://{YaGptConfig.catalog_id}/yandexgpt",
+async def yagpt(query):
+    prompt = {"modelUri": f"gpt://{config.ya_gpt.catalog_id}/yandexgpt",
               "completionOptions": {"stream": False, "temperature": 0.1, "maxTokens": "2000"},
               "messages": [{"role": "user", "text": query},
                            {"role": "system",
@@ -26,10 +28,9 @@ def yagpt(query):
                                     " или одно значение если оно одно"}]}
 
     url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
-    headers = {"Content-Type": "application/json", "Authorization": f"Api-Key {YaGptConfig.api_key}"}
+    headers = {"Content-Type": "application/json", "Authorization": f"Api-Key {config.ya_gpt.api_key}"}
 
-    response = requests.post(url, headers=headers, json=prompt)
-    result = response.text
-    with open('./log.txt', 'a') as f:
-        f.write(result)
-    return result
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, headers=headers, json=prompt) as response:
+            result = await response.text()
+            return result
