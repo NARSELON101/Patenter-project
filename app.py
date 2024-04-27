45814cad1398c1887b1a97d9ed3ef0ba4b5b8b06
Филиппов Данил <filippok2004180@gmail.com>
2024-04-27 11:48:32 +0500

    Поднял уровень логов до debug

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ import asyncio
 import logging
 
 from handlers.user import register_user
+from loader import bot, dp, storage, config
 from middlewares.environment import EnvironmentMiddleware
-from loader import bot, config, dp
 from utils.database import create_db
 
 logger = logging.getLogger(__name__)
 
+
 def register_all_middlewares(dp, config):
     dp.setup_middleware(EnvironmentMiddleware(config=config))
+
 
 def register_all_handlers(dp):
     register_user(dp)
@@ -17,12 +19,12 @@ def register_all_handlers(dp):
 
 async def main():
     logging.basicConfig(
-        level=logging.INFO,
+        level=logging.DEBUG if config.get('debug', False) else logging.INFO,
         format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
     )
     logger.info("Starting bot")
 
-    #register_all_middlewares(dp, config)
+    register_all_middlewares(dp, config)
     register_all_handlers(dp)
     await create_db()
     try:
